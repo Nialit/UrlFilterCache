f5a058837298262a7fc8bc506200ed200915ecc3
Natal Kaplya <n.kaplya@it-constanta.ru>
2016-02-20 11:46:34 +0300

    -Rest facade migrated to Jersey 2 -Programmatic enabling rest facade through configuration added
    -Pom changed

--- src/main/java/ru/cache/rest/DefaultWSInitializer.java
+++ src/main/java/ru/cache/rest/DefaultWSInitializer.java
@@ -4,32 +4,42 @@
  */
 package ru.cache.rest;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.core.MediaType;
+
+import org.glassfish.jersey.process.Inflector;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.model.Resource;
+import org.glassfish.jersey.server.model.ResourceMethod;
 
 /**
- *
  * @author Natal Kaplya
  */
 @ApplicationPath("cache") // set the path to REST web services
-public class DefaultWSInitializer extends Application {
+public class DefaultWSInitializer extends ResourceConfig {
 	private static boolean isActive = false;
+
+	public DefaultWSInitializer() {
+		if (isActive) {
+			Resource.Builder resourceBuilder = Resource.builder(CacheRestFacade.class).path("");
+			final Resource resource = resourceBuilder.build();
+			registerResources(resource);
+		}
+	}
 
 	public static void setCacheRestActive(boolean active) {
 		isActive = active;
 	}
 
-	@Override
-	public Set<Class<?>> getClasses() {
-		if (isActive) {
-			final Set<Class<?>> classes = new HashSet<Class<?>>();
-			// register root resource
-			classes.add(CacheRestFacade.class);
-			return classes;
-		} else
-			return Collections.emptySet();
-	}
+	// @Override
+	// public Set<Class<?>> getClasses() {
+	// if (isActive) {
+	// final Set<Class<?>> classes = new HashSet<Class<?>>();
+	// // register root resource
+	// classes.add(CacheRestFacade.class);
+	// return classes;
+	// } else
+	// return Collections.emptySet();
+	// }
 }
